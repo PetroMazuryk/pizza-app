a44c5098c2e9a9bba7f9079a965dce11804b082e
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-07-22 20:58:13 +0300

    card rendering has been changed

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,22 +1,35 @@
 import scss from './PizzaItem.module.scss';
 
-const PizzaItem = ({ imageUrl, title, price }) => (
-  <li>
-    <img className={scss.img} src={imageUrl} alt={title} />
-    <div className={scss.title}>{title}</div>
-    <div className={scss.selector}>
-      <ul>
-        <li className={scss.active}>тонке</li>
-        <li>традиційне</li>
-      </ul>
-      <ul>
-        <li>{price} см</li>
-        <li>30 см</li>
-        <li>40 см</li>
-      </ul>
+const PizzaItem = ({ imageUrl, title, price, sizes, types }) => {
+  const typenames = ['тонке', 'традиційне'];
+  return (
+    <div className={scss.wrapper}>
+      <div className={scss.wrapperImg}>
+        <img className={scss.img} src={imageUrl} alt={title} />
+      </div>
+      <div className={scss.title}>{title}</div>
+      <div className={scss.selector}>
+        <ul className={scss.listTypes}>
+          {types.map((typeId, index) => (
+            <li className={scss.itemSizes} key={index}>
+              {typenames[typeId]}
+            </li>
+          ))}
+        </ul>
+        <ul className={scss.listSizes}>
+          {sizes.map((size, index) => (
+            <li className={scss.itemSizes} key={index}>
+              {size} см.
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={scss.wrapperPrice}>
+        <div> {price} грн.</div>
+        <button>Додати</button>
+      </div>
     </div>
-    <button>Counter</button>
-  </li>
-);
+  );
+};
 
 export default PizzaItem;
--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,12 +1,22 @@
 import PizzaItem from '../PizzaItem/PizzaItem';
 import scss from './PizzaList.module.scss';
 
-const PizzaList = ({ items }) => (
-  <ul className={scss.wrapper}>
-    {items.map(({ id, imageUrl, title, price }) => (
-      <PizzaItem key={id} imageUrl={imageUrl} title={title} price={price} />
-    ))}
-  </ul>
-);
+const PizzaList = ({ items }) => {
+  return (
+    <ul className={scss.wrapper}>
+      {items.map(({ id, imageUrl, title, price, sizes, types }) => (
+        <li key={id}>
+          <PizzaItem
+            imageUrl={imageUrl}
+            title={title}
+            price={price}
+            sizes={sizes}
+            types={types}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default PizzaList;
