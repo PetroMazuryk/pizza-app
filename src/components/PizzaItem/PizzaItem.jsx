9c14f38b8f437ba89c94032c8383509502199102
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-07-22 22:06:07 +0300

    added useState

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+import clsx from 'clsx';
 import scss from './PizzaItem.module.scss';
 
 const PizzaItem = ({ imageUrl, title, price, sizes, types }) => {
+  const [activeType, setActiveType] = useState(0);
+  const [activeSize, setActiveSize] = useState(0);
+
   const typenames = ['тонке', 'традиційне'];
+
   return (
     <div className={scss.wrapper}>
       <div className={scss.wrapperImg}>
@@ -10,15 +16,27 @@ const PizzaItem = ({ imageUrl, title, price, sizes, types }) => {
       <div className={scss.title}>{title}</div>
       <div className={scss.selector}>
         <ul className={scss.listTypes}>
-          {types.map((typeId, index) => (
-            <li className={scss.itemSizes} key={index}>
+          {types.map((typeId) => (
+            <li
+              key={typeId}
+              onClick={() => setActiveType(typeId)}
+              className={clsx(scss.itemTypes, {
+                [scss.active]: activeType === typeId,
+              })}
+            >
               {typenames[typeId]}
             </li>
           ))}
         </ul>
         <ul className={scss.listSizes}>
           {sizes.map((size, index) => (
-            <li className={scss.itemSizes} key={index}>
+            <li
+              key={index}
+              onClick={() => setActiveSize(index)}
+              className={clsx(scss.itemSizes, {
+                [scss.active]: activeSize === index,
+              })}
+            >
               {size} см.
             </li>
           ))}
