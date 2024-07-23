67fad62957403cbedfd81870d6b134e5bd817903
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-07-23 23:41:56 +0300

    added component SortPopap

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,11 +2,15 @@ import Categories from '../Categories/Categories';
 import PizzaList from '../PizzaList/PizzaList';
 import pizzas from '../../../src/helpers/pizzas.json';
 import scss from './Container.module.scss';
+import SortPopup from '../SortPopap/SortPopap';
 
 export const Container = () => {
   return (
     <div className={scss.container}>
-      <Categories />
+      <div className={scss.wrapper}>
+        <Categories />
+        <SortPopup />
+      </div>
 
       <PizzaList items={pizzas} />
     </div>
