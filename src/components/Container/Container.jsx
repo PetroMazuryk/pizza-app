cd350c80768326fbf1a863be75dd272308b0113d
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-07-31 23:26:26 +0300

    added react-router-dom

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Categories from '../Categories/Categories';
 import PizzaList from '../PizzaList/PizzaList';
+import Skeleton from '../Skeleton/Skeleton';
 
 import scss from './Container.module.scss';
 
@@ -10,11 +11,13 @@ import SortPopup from '../SortPopap/SortPopap';
 const BASE_URL = import.meta.env.VITE_API_TEST;
 export const Container = () => {
   const [items, setItems] = useState([]);
+  const [isloading, setIsloading] = useState(true);
 
   useEffect(() => {
     async function fetchAllPizzas() {
       const response = await axios.get(`${BASE_URL}/items`);
       setItems(response.data);
+      setIsloading(false);
     }
 
     fetchAllPizzas();
@@ -26,8 +29,11 @@ export const Container = () => {
         <Categories />
         <SortPopup />
       </div>
-
-      <PizzaList items={items} />
+      {isloading ? (
+        [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+      ) : (
+        <PizzaList items={items} />
+      )}
     </div>
   );
 };
