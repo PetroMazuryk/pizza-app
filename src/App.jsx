5bcbeead654ffc20cdfdf12e71465b0a29f6e9e9
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-08-01 23:16:40 +0300

    created the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { Container } from './components/Container/Container';
+
+const HomePage = lazy(() => import('./pages/Home'));
+const CartPage = lazy(() => import('./pages/Cart'));
+const NotFoundPage = lazy(() => import('./pages/NotFound'));
 
 const App = () => {
   return (
     <>
       <Container />
+      <Suspense fallback="Loading...">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
