cabd920ee3399b4fef3e5ea5a1b4849e8319807c
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-08-04 23:28:31 +0300

    added redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 import App from './App.jsx';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/pizza-app">
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
