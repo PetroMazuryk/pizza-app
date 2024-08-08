74ca65b156ad9e08bb692f6f92be16c54e3483e2
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-08-08 19:46:01 +0000

    Deploying to gh-pages from @ PetroMazuryk/pizza-app@098369569d1228f68b434be1836b69fddf17b5f0 🚀

---
+++ assets/Cart-563392c8.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-8e94da38.js";const s=()=>r.jsx("div",{children:"Cart"});export{s as default};
--- assets/Cart-c5d56204.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-92628ba6.js";const s=()=>r.jsx("div",{children:"Cart"});export{s as default};
