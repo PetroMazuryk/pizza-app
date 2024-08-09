72c349bbbcf854a5760baaed4f72664d28fdd894
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-08-09 23:57:54 +0300

    added input field validation

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,8 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeFilterSearch } from '../../redux/slices/filterSlice';
 import { selectFilter } from '../../redux/slices/selectors';
+import Joi from 'joi';
 
 import sprite from '../../assets/sprite.svg';
 import scss from './Search.module.scss';
@@ -10,14 +11,31 @@ const Search = () => {
   const dispatch = useDispatch();
   const inputRef = useRef();
   const inputValue = useSelector(selectFilter);
+  const [error, setError] = useState('');
+
+  const schema = Joi.string().min(3).max(20).messages({
+    'string.empty': 'Поле не може бути порожнім',
+    'string.min': 'Назва повинна містити щонайменше 3 символи',
+    'string.max': 'Назва не може перевищувати 20 символів',
+  });
 
   const handleChange = (event) => {
     const value = event.target.value;
+
+    const { error } = schema.validate(value);
+
+    if (error) {
+      setError(error.message);
+    } else {
+      setError('');
+    }
+
     dispatch(changeFilterSearch(value));
   };
 
   const handleClearClick = () => {
     dispatch(changeFilterSearch(''));
+    setError('');
   };
 
   return (
@@ -35,13 +53,13 @@ const Search = () => {
         ref={inputRef}
         name="search"
       />
+      {error && <p className={scss.errorText}>{error}</p>}
       {inputValue !== '' && (
         <svg
           onClick={handleClearClick}
           className={scss.iconClose}
           width="20"
-          height="20
-      "
+          height="20"
         >
           <use href={`${sprite}#icon-close`}></use>
         </svg>
