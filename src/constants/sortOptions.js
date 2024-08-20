6939b1f826dfae7f0209ab20e2e780e541975d19
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-08-21 00:16:39 +0300

    added sort ASC

--- src/constants/sortOptions.js
+++ src/constants/sortOptions.js
@@ -1,6 +1,7 @@
 export const sortOptions = [
   { name: 'популярностю', value: 'rating' },
-  { name: 'алфавітом', value: 'title' },
+  { name: 'алфавітом (DESC)', value: 'title' },
+  { name: 'алфавітом (ASC)', value: '-title' },
   { name: 'ціною ⬆', value: 'price' },
   { name: 'ціною ⬇', value: '-price' },
 ];
