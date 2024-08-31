8053543afc88092e27e6af76d2ca70e3ad2fa70a
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-09-01 01:39:00 +0300

    added pizza title

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,15 +1,23 @@
+import { useSelector } from 'react-redux';
+import { selectCategoryId } from '../../redux/slices/selectors';
+import { categoryOptions } from '../../constants/categoryOptions';
+
 import PizzaItem from '../PizzaItem/PizzaItem';
 import scss from './PizzaList.module.scss';
 
 const PizzaList = ({ items }) => {
+  const categoryId = useSelector(selectCategoryId);
   return (
-    <ul className={scss.wrapper}>
-      {items.map((pizza) => (
-        <li key={pizza.id}>
-          <PizzaItem {...pizza} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2 className={scss.pizzaTitle}>{categoryOptions[categoryId]}</h2>
+      <ul className={scss.wrapper}>
+        {items.map((pizza) => (
+          <li key={pizza.id}>
+            <PizzaItem {...pizza} />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
