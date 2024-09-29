d8ee7f1a99eb70a1b1c470ddc129f3cb30a48af1
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-09-29 20:14:28 +0000

    Deploying to gh-pages from @ PetroMazuryk/pizza-app@3cc0e09da70837faa5b14934d25ba0da7a978cbd 🚀

--- assets/DocumentTitle-2eb2cadb.js
+++
@@ -1,1 +0,0 @@
-import{j as t,H as s}from"./index-210313ce.js";const o=({children:e})=>t.jsx(s,{children:t.jsx("title",{children:e})});export{o as D};
---
+++ assets/DocumentTitle-2ec36278.js
@@ -0,0 +1,1 @@
+import{j as t,H as s}from"./index-074958fa.js";const o=({children:e})=>t.jsx(s,{children:t.jsx("title",{children:e})});export{o as D};
